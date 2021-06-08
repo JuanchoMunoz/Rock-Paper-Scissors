693bc84c5973e77d2d55e48ba6bd16294095d6f2
Juancho <juanchomnz11@gmail.com>
2021-06-08 12:38:38 -0400

    Streak counter and some styling elements added

--- script.js
+++ script.js
@@ -53,9 +53,17 @@ function playerSelection(){
 
   //creates a paragraph element in the div
 let matchResultp = document.createElement("p");
+matchResultp.style.color = 'whitesmoke';
 matchResultp.setAttribute("id", "pogy");
+let appendStreak = document.getElementById('streakappendhere')
+//creating counter for our streak and w/l
+
+let wins = 0;
+let losses= 0; 
+let streak= 0; 
 
 //single instance of the game
+
 function jankenpon(userc)
 {
     let userchoice = userc; 
@@ -68,13 +76,15 @@ function jankenpon(userc)
        result = ('You win! Your' + '\xa0' + userc + '\xa0' + 'won against ' + computerchoice);
        console.log(result)
                  //attaches that text node to our new p element
-              
+                wins +=1;
+                streak+=1;
     } else if(userc == computerchoice){
-        result = ('You Tied ');
+        result = ('You Tied');
 
     } else{
         result = ('You Lose! Your' + '\xa0' + userc + '\xa0' + 'lost against ' + computerchoice);
-
+        losses+=1
+        streak = 0;
     }
 
 
@@ -84,6 +94,7 @@ function jankenpon(userc)
     if(document.body.contains(document.getElementById('pogy'))){
         console.log('pogy Exists, remove mathresulttchild');
         matchResultp.innerHTML=" ";
+        appendStreak.innerHTML= " ";
     } else{
         console.log('pogy doesnt exist');
     
@@ -93,11 +104,22 @@ function jankenpon(userc)
         let matchResultt= document.createTextNode(result);
      matchResultp.appendChild(matchResultt);
          //identifies an element to put the new element 
-         var element = document.getElementById("userchoice");
+         var element = document.getElementById("appendhere");
             //appends that NEW PARAHRAPH ELEMENT to a existing element 
             element.appendChild(matchResultp); 
             //thisinterestsme.com/check-element-exists-javascript
 
+            //win lose caalculation
+            let ratio = (wins/losses);
+            if(ratio==NaN||ratio==Infinity){
+                ratio=0;
+            }
+            ratio = (ratio*100)/100;
+            ratio = Math.round(ratio*100)/100;
+                        //create a streak element counter and winrate:
+            let streakresult = document.createTextNode("Your Current Streak is " + streak + " With A Win/Lose Ratio of " + ratio );
+          
+            appendStreak.appendChild(streakresult);
             console.log(computerchoice);
 }
 
@@ -111,17 +133,20 @@ let btns = document.querySelector('#scissors');
 let btnp =  document.querySelector('#paper');
 let btnr = document.querySelector('#rock');
 
-btns.addEventListener('click', function(){
+btns.addEventListener('click', function(e){
+    e.preventDefault();
     jankenpon(btns.value);
     console.log(btns.value);
 });
 
-btnp.addEventListener('click', function(){
+btnp.addEventListener('click', function(e){
+    e.preventDefault();
     jankenpon(btnp.value);
     console.log(btnp.value);
 });
 
-btnr.addEventListener('click', function(){
+btnr.addEventListener('click', function(e){
+    e.preventDefault();
     jankenpon(btnr.value);
     console.log(btnr.value);
 });
